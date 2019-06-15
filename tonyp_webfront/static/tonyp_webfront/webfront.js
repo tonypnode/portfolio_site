40febc4358f1a20bf30689dc054dd3fd55356118
TonyPnode <sgtp@me.com>
2019-06-15 14:25:19 -0400

    WIP simplified /AJP//
    Signed-off-by: TonyPnode <sgtp@me.com>

--- tonyp_webfront/static/tonyp_webfront/webfront.js
+++
@@ -1,8 +0,0 @@
-function topNavResponse() {
-  var x = document.getElementById("TP-TopNav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
