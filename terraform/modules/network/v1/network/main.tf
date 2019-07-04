7d1ccb2e4a32713e6daf46d22a797033dc43649a
tonypnode <sgtp@me.com>
2019-07-03 22:07:16 -0400

    change subnet resource name /AJP//
    Signed-off-by: tonypnode <sgtp@me.com>

--- terraform/modules/network/v1/network/main.tf
+++ terraform/modules/network/v1/network/main.tf
@@ -1,6 +1,6 @@
 # AZ data from https://console.aws.amazon.com/vpc/home?region=us-east-1#subnets:sort=SubnetId
 
-resource "aws_subnet" "subnet_az1" {
+resource "aws_subnet" "subnet_az" {
   #Outputs from VPC
   vpc_id = "${var.vpc_id}"
   cidr_block = "${var.cidr_block_az}"
