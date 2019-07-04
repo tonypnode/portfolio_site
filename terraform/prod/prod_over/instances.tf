a3d11a8ae21050d08ec014e567acf887451bd18c
tonypnode <sgtp@me.com>
2019-07-03 22:07:53 -0400

    add webbox info from under state /AJP//
    Signed-off-by: tonypnode <sgtp@me.com>

--- terraform/prod/prod_over/instances.tf
+++ terraform/prod/prod_over/instances.tf
@@ -2,7 +2,7 @@ module "web_front1" {
   source = "../../modules/ec2/v1/webbox"
 
 
-  vpc_elb_ip = ""
-  keyname = ""
-  vpc_id = ""
+  vpc_elb_ip = "${data.terraform_remote_state.prod_under.lb.lb_internal_ip}"
+  keyname = "${var.key_name}"
+  vpc_id = "${data.terraform_remote_state.prod_under.tonyp_info_vpc.vpc_id}"
 }
